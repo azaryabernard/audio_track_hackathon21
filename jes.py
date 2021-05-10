e705bfeeaaa893d40e0bc5b25663da1830f40649
Brigitta Jesica <brigittajesica@Brigittas.local>
2021-05-10 13:54:50 +0200

    towardsdatasciencecopas

---
+++ jes.py
@@ -0,0 +1,190 @@
+#### Dependencies ####
+import IPython.display as ipd
+import numpy as np
+import pandas as pd
+import librosa
+import librosa.display
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import struct
+from scipy.io import wavfile as wav
+import os
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from sklearn import metrics 
+from sklearn.preprocessing import LabelEncoder
+from keras.utils import to_categorical
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, Conv2D, MaxPooling2D, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from sklearn import metrics 
+#### Dependencies ####
+
+def read_file_properties(filename):
+
+        wave_file = open(filename,"rb")
+        
+        riff = wave_file.read(12)
+        fmt = wave_file.read(36)
+        
+        num_channels_string = fmt[10:12]
+        num_channels = struct.unpack('<H', num_channels_string)[0]
+
+        sample_rate_string = fmt[12:16]
+        sample_rate = struct.unpack("<I",sample_rate_string)[0]
+        
+        bit_depth_string = fmt[22:24]
+        bit_depth = struct.unpack("<H",bit_depth_string)[0]
+
+        return (num_channels, sample_rate, bit_depth)
+    
+
+
+df = pd.read_csv('UrbanSound8K/metadata/UrbanSound8K.csv')
+
+# Get all labels
+labels = list(df['class'].unique())
+
+files = dict()
+for i in range(len(labels)):
+    tmp = df[df['class'] == labels[i]][:1].reset_index()
+    path = 'UrbanSound8K/audio/fold{}/{}'.format(tmp['fold'][0], tmp['slice_file_name'][0])
+    files[labels[i]] = path
+    
+fig = plt.figure(figsize=(15,15))
+fig.subplots_adjust(hspace=0.4, wspace=0.4)
+for i, label in enumerate(labels):
+    fn = files[label]
+    fig.add_subplot(5, 2, i+1)
+    plt.title(label)
+    data, sample_rate = librosa.load(fn)
+    _ = librosa.display.waveplot(data, sr= sample_rate)
+plt.savefig('class_examples.png')
+
+
+# Log audio files to Comet for debugging
+for label in labels:
+    fn = files[label]
+
+    
+
+audiodata = []
+for index, row in df.iterrows():
+    fn = 'UrbanSound8K/audio/fold{}/{}'.format(row['fold'], row['slice_file_name'])
+    data = read_file_properties(fn)
+    audiodata.append(data)
+
+# Convert to dataframe
+audiodf = pd.DataFrame(audiodata, columns=['num_channels', 'sample_rate', 'bit_depth'])
+
+# Extract MFCC features
+fn = 'UrbanSound8K/audio/fold1/191431-9-0-66.wav'
+librosa_audio, librosa_sample_rate = librosa.load(fn)
+scipy_sample_rate, scipy_audio = wav.read(fn)
+mfccs = librosa.feature.mfcc(y=librosa_audio, sr=librosa_sample_rate, n_mfcc = 40)
+
+plt.figure(figsize=(8,8))
+librosa.display.specshow(mfccs, sr=librosa_sample_rate, x_axis='time')
+plt.savefig('MFCCs.png')
+
+def extract_features(file_name):
+   
+    try:
+        audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+        mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+        mfccsscaled = np.mean(mfccs.T,axis=0)
+        
+    except Exception as e:
+        print("Error encountered while parsing file: ", file)
+        return None 
+     
+    return mfccsscaled
+
+
+
+# Set the path to the full UrbanSound dataset 
+fulldatasetpath = 'UrbanSound8K/audio/'
+
+metadata = df
+
+features = []
+
+# Iterate through each sound file and extract the features 
+for index, row in metadata.iterrows():
+    
+    file_name = os.path.join(os.path.abspath(fulldatasetpath),'fold'+str(row["fold"])+'/',str(row["slice_file_name"]))
+    
+    class_label = row["class"]
+    data = extract_features(file_name)
+    
+    features.append([data, class_label])
+
+# Convert into a Panda dataframe 
+featuresdf = pd.DataFrame(features, columns=['feature','class_label'])
+
+
+# Convert features and corresponding classification labels into numpy arrays
+X = np.array(featuresdf.feature.tolist())
+y = np.array(featuresdf.class_label.tolist())
+
+# Encode the classification labels
+le = LabelEncoder()
+yy = to_categorical(le.fit_transform(y))
+
+
+
+# split the dataset 
+from sklearn.model_selection import train_test_split 
+
+x_train, x_test, y_train, y_test = train_test_split(X, yy, test_size=0.2, random_state = 42)
+
+
+num_labels = yy.shape[1]
+filter_size = 2
+
+# Construct model 
+model = Sequential()
+
+model.add(Dense(256, input_shape=(40,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(256))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
+
+
+from keras.callbacks import ModelCheckpoint 
+from datetime import datetime 
+
+num_epochs = 100
+num_batch_size = 32
+
+start = datetime.now()
+
+model.fit(x_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(x_test, y_test), verbose=1)
+
+duration = datetime.now() - start
+print("Training completed in time: ", duration)
+
+# Evaluating the model on the training and testing set
+score = model.evaluate(x_train, y_train, verbose=0)
+
+score = model.evaluate(x_test, y_test, verbose=0)
