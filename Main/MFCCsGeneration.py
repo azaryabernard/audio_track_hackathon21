5cb6025eaf15389b3dbc372049ad55a72e26a0f9
alvandi <geradylanalvandi@gmail.com>
2021-05-10 18:55:49 +0200

    pingu

--- Main/MFCCsGeneration.py
+++ Main/MFCCsGeneration.py
@@ -1,8 +1,24 @@
+#### Dependencies ####
+import IPython.display as ipd
+import numpy as np
+import pandas as pd
+import librosa
+import librosa.display
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.use('agg')
+import tensorflow as tf
+from scipy.io import wavfile as wav
+#### Dependencies ####
+
 fn = 'UrbanSound8K/audio/fold9/7975-3-0-0.wav'
 librosa_audio, librosa_sample_rate = librosa.load(fn)
 scipy_sample_rate, scipy_audio = wav.read(fn)
 mfccs = librosa.feature.mfcc(y=librosa_audio, sr=librosa_sample_rate, n_mfcc=40)
 
-plt.figure(figsize=(8, 8))
-librosa.display.specshow(mfccs, sr=librosa_sample_rate, x_axis='time')
-plt.savefig('MFCCs.png')
+print(mfccs)
+print(mfccs.shape)
+
+print("END OF SESSION")
+
+
