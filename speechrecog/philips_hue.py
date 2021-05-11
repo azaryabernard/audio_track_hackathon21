29e329e806d4123b538b8b92050e2362ff8f7904
Azarya Bernard <azarya.de@gmail.com>
2021-05-11 19:35:34 +0200

    updated

--- speechrecog/philips_hue.py
+++ speechrecog/philips_hue.py
@@ -1,8 +1,11 @@
 import requests
 import json
 import urllib
-
 from requests.api import request
+from rgbxy import Converter
+import webcolors
+
+converter = Converter()
 
 bridge_ip = "192.168.0.2"
 bridge_username = "soduBL8lu551zb71pFnoxwZFAgoCzcPWvk1z9K4g"
@@ -32,7 +35,6 @@ def set_brightness_group(where, bright):
     group_id = groups[where]
 
     bri = bright / 100 * 254
-    print(bri)
     payload = {"bri": int(bri)}
     headers = {'content-type': 'application/json'}
     r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
@@ -49,6 +51,21 @@ def decrease_brightness_group(where):
 def increase_brightness_group(where):
     bri = get_brightness_group(where)
     set_brightness_group(where, bri + 20)
+
+def set_color(where, color):
+    group_id = groups[where]
+    xy = [0, 0]
+    if color:
+        clr = webcolors.name_to_hex(color).split('#')[1]
+        print(clr)
+        if clr and clr != '000000':
+            xy = converter.hex_to_xy(clr)
+            payload = {"xy": xy}
+            headers = {'content-type': 'application/json'}
+            r = requests.put("http://"+bridge_ip+"/api/"+bridge_username+"/groups/"+str(group_id)+"/action", data=json.dumps(payload), headers=headers)
+
+   
+
 
 #turn_on_group('lights')
 # {"1":{"name":"Azar","lights":["4","3","2","1"],"sensors":[],"type":"Room","state":{"all_on":true,"any_on":true},"recycle":false,"class":"Bedroom","action":{"on":true,"bri":254,"hue":50996,"sat":42,"effect":"none","xy":[0.3616,0.3315],"ct":222,"alert":"none","colormode":"xy"}},"2":{"name":"Entertainment-Bereich 1","lights":["1","2"],"sensors":[],"type":"Entertainment","state":{"all_on":true,"any_on":true},"recycle":false,"class":"TV","stream":{"proxymode":"auto","proxynode":"/lights/1","active":false,"owner":null},"locations":{"1":[-0.01,0.29,0.00],"2":[0.00,1.00,0.00]},"action":{"on":true,"bri":254,"hue":8402,"sat":140,"effect":"none","xy":[0.4575,0.4099],"ct":366,"alert":"none","colormode":"xy"}},"3":{"name":"Entertainment-Bereich 2","lights":["2"],"sensors":[],"type":"Entertainment","state":{"all_on":true,"any_on":true},"recycle":false,"class":"TV","stream":{"proxymode":"auto","proxynode":"/lights/2","active":false,"owner":null},"locations":{"2":[0.00,1.00,0.00]},"action":{"on":true,"bri":254,"hue":8402,"sat":140,"effect":"none","xy":[0.4575,0.4099],"ct":366,"alert":"none","colormode":"xy"}}}
